defa85060a9ab6c29e4f29a2972177aedd9d9a31
Mohd Mustafeez Ahmad <105917441+Mustafeez001@users.noreply.github.com>
2022-12-16 21:11:26 +0530

    Add files via upload

---
+++ ONLINE_BANKING_SYSTEM/src/com/masai/Exception/AccountException.java
@@ -0,0 +1,19 @@
+package com.masai.Exception;
+
+public class AccountException extends Exception{
+
+	public AccountException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public AccountException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	
+}
